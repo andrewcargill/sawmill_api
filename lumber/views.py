941452d06c35e21d53bdbafb78aaf3832eb886ca
andrewcargill <andycargill01@aol.com>
2023-06-13 19:31:33 +0200

    set up working test api

--- lumber/views.py
+++ lumber/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Test
+from .serializers import TestSerializer
 
-# Create your views here.
+class TestList(generics.ListCreateAPIView):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
+
--- sawmill_api/urls.py
+++ sawmill_api/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lumber.views import TestList
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/lumber/', TestList.as_view(), name='lumber-api'),
 ]
