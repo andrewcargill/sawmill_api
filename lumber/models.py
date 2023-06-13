7acce9fc3198d249d4d7b15649a34cbb36b1afcc
andrewcargill <andycargill01@aol.com>
2023-06-13 19:07:53 +0200

    change name of test database

--- lumber/models.py
+++ lumber/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Person(models.Model):
+class Test(models.Model):
     data1 = models.CharField(max_length=100)
     data2 = models.IntegerField()
     data3 = models.CharField(max_length=100)
