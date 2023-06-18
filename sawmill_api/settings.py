faf33d58553bf8177f6cfbf7e7c7d2d39ce01bea
andrewcargill <andycargill01@aol.com>
2023-06-18 12:55:55 +0200

    update secret key

--- sawmill_api/settings.py
+++ sawmill_api/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'jeos1234lfds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
