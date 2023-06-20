0d3bbbd919cde18e580f7f58e002fb375c16ec3f
andrewcargill <andycargill01@aol.com>
2023-06-20 22:22:17 +0200

    adds models to admin view

--- lumber/admin.py
+++ lumber/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Test
+from .models import Test, Tree, Log, Plank, MoistureCheck
 
 admin.site.register(Test)
+admin.site.register(Tree)
+admin.site.register(Log)
+admin.site.register(Plank)
+admin.site.register(MoistureCheck)
+
