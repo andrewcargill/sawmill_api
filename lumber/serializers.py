39f0c833aeeb83d1276973bf4a8dbaa3648af360
andrewcargill <andycargill01@aol.com>
2023-06-21 10:31:53 +0200

    update to solve id search

--- lumber/serializers.py
+++ lumber/serializers.py
@@ -4,7 +4,7 @@ from .models import Test, Tree, Log, Plank, MoistureCheck
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
-        fields = '__all__'
+        fields = ['id', 'data1', 'data2', 'data3']
 
 class TreeSerializer(serializers.ModelSerializer):
     class Meta:
