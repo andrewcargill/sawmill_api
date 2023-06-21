6d74c70d76e528c09aaf98ef7e7e32d1288ff29e
andrewcargill <andycargill01@aol.com>
2023-06-21 07:36:50 +0200

    add search to test

--- lumber/views.py
+++ lumber/views.py
@@ -1,10 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Test, Tree, Log, Plank, MoistureCheck
 from .serializers import TestSerializer, TreeSerializer, LogSerializer, PlankSerializer, MoistureCheckSerializer
 
 class TestList(generics.ListCreateAPIView):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['data1', 'data3']
 
 class TestDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Test.objects.all()
