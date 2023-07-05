b5269397eb45f441751bf48d7b83611e7cb7bb99
andrewcargill <andycargill01@aol.com>
2023-07-05 22:54:38 +0200

    update serializers so tree info is reached from plank view

--- lumber/serializers.py
+++ lumber/serializers.py
@@ -11,20 +11,40 @@ class DropBoxFileSerializer(serializers.ModelSerializer):
         model = DropboxTest
         fields = '__all__'
 
+# class TreeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Tree
+#         fields = '__all__'
+
+# class LogSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Log
+#         fields = '__all__'
+
+# class PlankSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Plank
+#         fields = '__all__'
+
 class TreeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tree
-        fields = '__all__'
+        fields = ('id', 'species', 'date', 'reason_for_felling', 'age', 'longitude', 'latitude', 'lumberjack', 'image')
 
 class LogSerializer(serializers.ModelSerializer):
+    tree = TreeSerializer(read_only=True)
+
     class Meta:
         model = Log
-        fields = '__all__'
+        fields = ('id', 'length', 'tree', 'date', 'diameter', 'buck')
 
 class PlankSerializer(serializers.ModelSerializer):
+    log = LogSerializer()
+
     class Meta:
         model = Plank
-        fields = '__all__'
+        fields = ('id', 'log', 'date', 'width', 'depth', 'wood_grade', 'live_edge', 'furniture', 'structural', 'general', 'info', 'operator', 'image1', 'image2')
+
 
 class MoistureCheckSerializer(serializers.ModelSerializer):
     class Meta:
